e721f72e22aa2e64f5a9e514ed2b5eba14c8a7df
Cody Blackwell <codyblackwell67@gmail.com>
2019-05-08 13:06:02 -0400

    day three

---
+++ 03-Loops/01-forLoops.js
@@ -0,0 +1,33 @@
+//For Loops
+
+/*
+A loop is a programming tool that repeats a set of instructions until a specified condition is met.
+*/
+
+const vactionSpot = (['Florence','Copenhagen','Istanbul']);
+
+console.log(vactionSpot[0]);
+console.log(vacationSpot[1]);
+console.log(vacationSpot[2]);
+
+//Types of loops
+/*
+For statement
+Do while statements
+while statement
+labeled statement
+break statement
+continue statement
+For in statement
+For of statement
+*/
+
+for (let i=0; i<10; i++) {
+    console.log(i);
+}
+
+//Challenge
+
+for (let i=0; i<21; i+=2) {
+    console.log(i);
+}
